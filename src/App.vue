5093560f6e0fa2467e3f8f652d1c914e2ee8d946
Alex On <shuhia.on@gmail.com>
2022-01-20 15:17:52 +0100

    feat: profile

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Profile from "./components/profile/Profile.vue";
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Profile></Profile>
 </template>
 
 <style>
---
+++ src/components/profile/ProfileInformation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="profile-information">
+    <h2>About {{ name }}</h2>
+    <ul>
+      <li v-for="(key, value) in information" :key="key" class="information">
+        {{ key + ": " + value }}
+      </li>
+    </ul>
+    {{ name }}
+  </div>
+</template>
+
+<script setup>
+const name = "Trivia Game";
+const information = {
+  interests: "Gaming",
+  favoriteQuote: "Takes courage to give up",
+};
+</script>
+
+<style scoped>
+.information {
+  text-align: left;
+}
+</style>
