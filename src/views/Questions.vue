cc968471018c065ed580a8ccc6447c3369953bf2
Alex On <shuhia.on@gmail.com>
2022-01-22 10:03:19 +0100

    add restfiles

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -1,12 +1,14 @@
 <template>
   <p>Question 1</p>
+  <input v-model="answer" />
   <router-link to="/results"><button>Answer</button></router-link>
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
+import { ref } from "vue";
 const router = useRouter();
-
+const answer = ref("");
 const handleClick = () => {
   router.push("/results");
 };
--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,13 +1,30 @@
 <template>
-  <input type="username" />
+  <input v-model="inputs.username" type="username" />
+  <input v-model="inputs.difficulty" type="number" />
+  <input v-model="inputs.numberOfQuestions" type="number" />
+  <input v-model="inputs.category" type="text" />
   <button @click="handleClick">Start</button>
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
+import { reactive, ref } from "vue";
 const router = useRouter();
+const username = ref("");
+const difficulty = ref("");
+const numberOfQuestions = ref(1);
+const category = ref("");
+const inputs = reactive({
+  username,
+  difficulty,
+  numberOfQuestions,
+  category,
+});
 const handleClick = () => {
   router.push("/questions");
+  // Extract values from inputs
+  const values = { ...inputs };
+  console.log(values);
 };
 </script>
 
